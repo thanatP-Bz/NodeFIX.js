3f6e66513b3dafe93617c145439757920a04c055
Thanat <bztzn512@gmail.com>
2022-03-16 18:58:21 -0400

    handleError

--- starter/middleware/error-handler.js
+++ starter/middleware/error-handler.js
@@ -1,10 +1,25 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 
-module.exports = errorHandlerMiddleware
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let CustomError = {
+    //set default
+    StatusCode: err.StatusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    smg: err.message || "Something went wrong try again later",
+  };
+
+  /*  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  } */
+
+  if (err.code && err.code === 11000) {
+    CustomError.smg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    CustomError.StatusCode = 400;
+  }
+  /*  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err }) */
+  return res.status(CustomError.StatusCode).json({ smg: CustomError.smg });
+};
+
+module.exports = errorHandlerMiddleware;
