fc310c12608116e3bce7b9575e7399ce7ea7989d
Thanat <bztzn512@gmail.com>
2022-03-18 17:23:47 -0400

    handlingError

--- starter/app.js
+++ starter/app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -15,8 +22,19 @@ const jobRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(express.json());
 // extra packages
+app.use(express.json());
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
--- starter/middleware/error-handler.js
+++ starter/middleware/error-handler.js
@@ -1,25 +1,37 @@
-const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  let CustomError = {
+  let customError = {
     //set default
     StatusCode: err.StatusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    smg: err.message || "Something went wrong try again later",
+    msg: err.message || "Something went wrong try again later",
   };
 
   /*  if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   } */
+  //name error
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.StatusCode = 400;
+  }
+
+  //cast err
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id: ${err.value}`;
+    customError.StatusCode = 404;
+  }
 
   if (err.code && err.code === 11000) {
-    CustomError.smg = `Duplicate value entered for ${Object.keys(
+    customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`;
-    CustomError.StatusCode = 400;
+    customError.StatusCode = 400;
   }
-  /*  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err }) */
-  return res.status(CustomError.StatusCode).json({ smg: CustomError.smg });
+  /* return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err }); */
+  return res.status(customError.StatusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
