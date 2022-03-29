f6d0f11c60c65fb5e7d31dbab983d5ad889661f6
Thanat <bztzn512@gmail.com>
2022-03-29 15:40:44 -0400

    findJob

--- starter/controllers/jobs.js
+++ starter/controllers/jobs.js
@@ -1,24 +1,46 @@
 const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
-const {} = require("../errors");
+const { NotFoundError, BadRequestError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  res.send("getAll Job");
+  const job = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const getSingleJob = async (req, res) => {
-  res.send("getSingle Job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with ID: ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
-  console.log(req.body);
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
-  res.send("update Job");
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === "" && position === "") {
+    throw new BadRequestError("The company and position cannot be emtied");
+  }
 };
 const deleteJob = async (req, res) => {
   res.send("delete Job");
